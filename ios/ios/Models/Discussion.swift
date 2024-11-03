700735933583257eda8293441be2b0632a6f6142
Vladimir Eremin <deytenit@Vladimirs-iMac-Pro.local>
2024-11-03 22:36:01 +0300

    iOS Project Base

---
+++ ios/ios/Models/Discussion.swift
@@ -0,0 +1,12 @@
+//
+//  Discussion.swift
+//  Nightwind
+//
+//  Created by Nightwind Development on 10/26/24.
+//
+
+import Foundation
+
+struct Discussion: Codable {
+
+}
---
+++ ios/ios/Models/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  Nightwind
+//
+//  Created by Nightwind Development on 10/26/24.
+//
+
+import Foundation
+
+struct Post: Codable {
+
+}
